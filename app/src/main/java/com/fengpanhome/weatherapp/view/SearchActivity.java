660c287613b781219eae9d7d09d4f4c581a68edc
Feng Pan <panfeng0007@gmail.com>
2016-08-25 02:55:17 -0400

    minor fix

--- app/src/main/java/com/fengpanhome/weatherapp/view/SearchActivity.java
+++ app/src/main/java/com/fengpanhome/weatherapp/view/SearchActivity.java
@@ -7,19 +7,13 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
-import android.widget.Switch;
-
 import com.fengpanhome.weatherapp.R;
 
 
-public class SearchActivity extends Activity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, AutoCompleteTextView.OnKeyListener
+public class SearchActivity extends Activity implements View.OnClickListener, AutoCompleteTextView.OnKeyListener
 {
 
     private AutoCompleteTextView inputTextView;
@@ -27,17 +21,6 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
 
     private Button fButton;
     private Button cButton;
-
-    private CheckBox queryMetar;
-    private CheckBox queryTaf;
-    private CheckBox queryPirep;
-    private CheckBox queryAreaForecast;
-
-    private boolean pilotMode;
-    private boolean isCheckingMetar;
-    private boolean isCheckingTaf;
-    private boolean isCheckingPirep;
-    private boolean isCheckingAreaForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +39,8 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
 
         fButton = (Button)findViewById(R.id.f_button);
         cButton = (Button)findViewById(R.id.c_button);
-        fButton.setText((char) 0x00b0 + "F");
-        cButton.setText((char) 0x00b0 + "C");
-
-        Switch pilotModeToggle = (Switch)findViewById(R.id.is_pilot_toggle);
-        pilotModeToggle.setOnCheckedChangeListener(this);
+        fButton.setText(R.string.fahrenheit);
+        cButton.setText(R.string.celsius);
 
         fButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.White));
         cButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.White));
@@ -68,20 +48,6 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
         fButton.setOnClickListener(this);
         cButton.setOnClickListener(this);
 
-        queryMetar = (CheckBox)findViewById(R.id.query_metar);
-        queryMetar.setOnCheckedChangeListener(this);
-        queryTaf = (CheckBox)findViewById(R.id.query_taf);
-        queryTaf.setOnCheckedChangeListener(this);
-        queryPirep = (CheckBox)findViewById(R.id.query_pirep);
-        queryPirep.setOnCheckedChangeListener(this);
-        queryAreaForecast = (CheckBox)findViewById(R.id.query_area_forecast);
-        queryAreaForecast.setOnCheckedChangeListener(this);
-
-        pilotMode = false;
-        isCheckingMetar = false;
-        isCheckingTaf = false;
-        isCheckingPirep = false;
-        isCheckingAreaForecast = false;
     }
 
     @Override
@@ -96,11 +62,6 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
                 args.putString("ACTIVITY", "SearchActivity");
                 args.putString("LOCATION", inputTextView.getText().toString());
                 args.putString("UNIT", unit);
-                args.putBoolean("PILOT_MODE", pilotMode);
-                args.putBoolean("CHECKING_METAR", isCheckingMetar);
-                args.putBoolean("CHECKING_TAF", isCheckingTaf);
-                args.putBoolean("CHECKING_PIREP", isCheckingPirep);
-                args.putBoolean("CHECKING_AREA_FORECAST", isCheckingAreaForecast);
                 intent.putExtras(args);
                 startActivity(intent);
                 this.finish();
@@ -137,84 +98,6 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-    {
-        switch (buttonView.getId())
-        {
-
-            case R.id.is_pilot_toggle:
-            {
-                final Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
-                final Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out);
-                if (isChecked)
-                {
-                    pilotMode = true;
-
-                    queryMetar.setVisibility(View.VISIBLE);
-                    queryMetar.setAnimation(fadeIn);
-
-                    queryTaf.setVisibility(View.VISIBLE);
-                    queryTaf.setAnimation(fadeIn);
-
-                    queryPirep.setVisibility(View.VISIBLE);
-                    queryPirep.setAnimation(fadeIn);
-
-                    queryAreaForecast.setVisibility(View.VISIBLE);
-                    queryAreaForecast.setAnimation(fadeIn);
-
-                    inputTextView.setHint(R.string.hint_airport_code);
-
-                }
-                else
-                {
-                    pilotMode = false;
-
-                    queryMetar.setVisibility(View.INVISIBLE);
-                    queryMetar.setAnimation(fadeOut);
-
-                    queryTaf.setVisibility(View.INVISIBLE);
-                    queryTaf.setAnimation(fadeOut);
-
-                    queryPirep.setVisibility(View.INVISIBLE);
-                    queryPirep.setAnimation(fadeOut);
-
-                    queryAreaForecast.setVisibility(View.INVISIBLE);
-                    queryAreaForecast.setAnimation(fadeOut);
-
-                    inputTextView.setHint(R.string.hint_location);
-                    isCheckingMetar = false;
-                    isCheckingTaf = false;
-                    isCheckingPirep = false;
-                    isCheckingAreaForecast = false;
-                }
-                break;
-            }
-            case R.id.query_metar:
-            {
-                isCheckingMetar = isChecked;
-                break;
-            }
-            case R.id.query_taf:
-            {
-                isCheckingTaf = isChecked;
-                break;
-            }
-            case R.id.query_pirep:
-            {
-                isCheckingPirep = isChecked;
-                break;
-            }
-            case R.id.query_area_forecast:
-            {
-                isCheckingAreaForecast = isChecked;
-                break;
-            }
-            default:
-                break;
-        }
-    }
-
-    @Override
     public boolean onKey(View v, int keyCode, KeyEvent event)
     {
         if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() != KeyEvent.ACTION_DOWN)
@@ -224,10 +107,6 @@ public class SearchActivity extends Activity implements View.OnClickListener, Co
             args.putString("ACTIVITY", "SearchActivity");
             args.putString("LOCATION", inputTextView.getText().toString());
             args.putString("UNIT", unit);
-            args.putBoolean("CHECKING_METAR", isCheckingMetar);
-            args.putBoolean("CHECKING_TAF", isCheckingTaf);
-            args.putBoolean("CHECKING_PIREP", isCheckingPirep);
-            args.putBoolean("CHECKING_AREA_FORECAST", isCheckingAreaForecast);
             intent.putExtras(args);
             startActivity(intent);
             this.finish();
